301b190c5cb3a690a2d6e523b3d0bb65e244c151
加藤　優奈 <yuvernacrew@gmail.com>
2017-09-30 05:01:08 +0900

    add sponser map

--- js/sponser.js
+++ js/sponser.js
@@ -1,73 +1,100 @@
 $(function(){
-  // 協賛データ
-  var sponser = [
-    // {
-    //   name : '海の台所 はまきん',
-    //   address : '愛知県名古屋市千種区池下2-2-15',
-    //   time : `17:00～24:00（平日）<br>
-    //   16:00～24:00（土・日・祝日）`,
-    //   tell: `0527517021`,
-    //   description : `日本酒やワインが豊富☆<br>
-    //   他では手に入らないプレミアム日本酒も多数取り揃えています☆その他、こだわりの焼酎、ワインも種類豊富！<br>
-    //   新鮮なお刺身と創作料理<br>
-    //   毎日オーナーが厳選して仕入れした魚が◎その他、パスタやお肉料理など種類豊富なお料理が楽しめます♪<br>
-    //   個室も完備！池下駅すぐ♪<br>
-    //   限定1室の掘りごたつ個室は6名様までOK☆駅近で終電ギリギリまで飲んでもOK！2軒目にも◎毎日深夜1時迄営業`,
-    //   label : 'バイト募集',
-    //   other : null,
-    //   logo : 'logo-hama.png',
-    //   images : ['hama1.png','hama2.jpg']
-    // },
-    // {
-    //   name : '団十郎',
-    //   address : '春日井市白山町4-8-2',
-    //   time : `17:00〜23:00(L.O22:30)<br>
-    //   日曜のみ12:00〜23:00(L.O22:30)<br>
-    //   定休日：第1.3水曜日`,
-    //   tell: `0568515003`,
-    //   description : `
-    //   <a href="http://www.danjuro29.com">ホームページ</a>
-    //   <a href="https://s.tabelog.com/aichi/A2303/A230301/23036018/">食べログ</a>
-    //   <a href="https://www.hotpepper.jp/strJ000414804/">ホットペッパー</a>`,
-    //   label : null,
-    //   other : null,
-    //   logo : "logo-dan.png",
-    //   images : ['dan-1.png','dan-2.png']
-    // },
-    // {
-    //   name : '団十郎',
-    //   address : '春日井市白山町4-8-2',
-    //   time : `17:00〜23:00(L.O22:30)<br>
-    //   日曜のみ12:00〜23:00(L.O22:30)<br>
-    //   定休日：第1.3水曜日`,
-    //   tell: `0568515003`,
-    //   description : `
-    //   <a href="http://www.danjuro29.com">ホームページ</a>
-    //   <a href="https://s.tabelog.com/aichi/A2303/A230301/23036018/">食べログ</a>
-    //   <a href="https://www.hotpepper.jp/strJ000414804/">ホットペッパー</a>`,
-    //   label : ,
-    //   other : ,
-    //   logo : ,
-    //   images :
-    // }
-  ]
+  var map;
+  var marker = [];
+  var infoWindow = [];
+  var markerData = [ // マーカーを立てる場所名・緯度・経度
+    {
+      name: '団十郎',
+      lat: 35.271214,
+      lng: 137.030245
+    },
+    {
+      name: '海の台所 はまきん',
+      lat: 35.168333,
+      lng: 136.945717
+    },
+    {
+      name: 'かどまつ今池店',
+      lat: 35.1693,
+      lng: 136.938734
+    },
+    {
+      name: 'カプリチョーザイオンモール岡崎店',
+      lat: 34.937846,
+      lng: 137.164454
+    },
+    {
+      name: '株式会社アトリエキース',
+      lat: 35.186693,
+      lng: 136.975899
+    },
+    {
+      name: '焼肉 龍華園',
+      lat: 35.171108,
+      lng: 136.947594
+    },
+    {
+      name: '早乙女ふるまゐ',
+      lat: 35.169956,
+      lng: 136.934814
+    },
+    {
+      name: 'ガーデンレストラン徳川園 蘇山荘',
+      lat: 35.18415,
+      lng: 136.932434
+    },
+    {
+      name: '点-TOMORU- 名駅太閤口店',
+      lat: 35.169232,
+      lng: 36.881203
+    },
+    {
+      name: 'じーる',
+      lat: 35.003796,
+      lng: 137.031446
+    },
+    {
+      name: '麺屋 ささき',
+      lat: 35.191327,
+      lng: 136.943516
+    },
+    {
+      name: '錦三山車楼',
+      lat: 35.171879,
+      lng: 136.902648
+    }
+  ];
 
-  // popcorn-nav
-  for(var i = 0 ; i < sponser.length ; i++){
-    var name = sponser[i].name;
-    $('.container').append(`<a class="modal-btn" id='${i}' href='#modal'>${name}</a>`);
-  };
+  function initMap() {
+   // 地図の作成
+      var mapLatLng = new google.maps.LatLng({lat: markerData[0]['lat'], lng: markerData[0]['lng']}); // 緯度経度のデータ作成
+     map = new google.maps.Map(document.getElementById('sponser-map'), { // #sampleに地図を埋め込む
+       center: mapLatLng, // 地図の中心を指定
+        zoom: 15 // 地図のズームを指定
+     });
 
-  //モーダルの中身
-  $('.modal-btn').click(function(){
-    //初期化
-    $('.remodal').children("p").remove();
-    var id = $(this).attr('id')
-    var name = sponser[id].name;
+   // マーカー毎の処理
+   for (var i = 0; i < markerData.length; i++) {
+          markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+          marker[i] = new google.maps.Marker({ // マーカーの追加
+           position: markerLatLng, // マーカーを立てる位置を指定
+              map: map // マーカーを立てる地図を指定
+         });
 
-    $('.remodal').append(
-      `<p>${name}</p>`
-    )
-  });
+       infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
+           content: '<div class="map-title">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
+         });
+
+       markerEvent(i); // マーカーにクリックイベントを追加
+   }
+  }
+
+  // マーカーにクリックイベントを追加
+  function markerEvent(i) {
+      marker[i].addListener('click', function() { // マーカーをクリックしたとき
+        infoWindow[i].open(map, marker[i]); // 吹き出しの表示
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initMap);
 
 });
