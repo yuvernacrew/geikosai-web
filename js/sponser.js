8cae0f233874f99e835419eadeedf0006c366517
加藤　優奈 <yuvernacrew@gmail.com>
2017-09-30 06:36:50 +0900

    edit map style

--- js/sponser.js
+++ js/sponser.js
@@ -1,4 +1,5 @@
 $(function(){
+
   var map;
   var marker = [];
   var infoWindow = [];
@@ -46,7 +47,7 @@ $(function(){
     {
       name: '点-TOMORU- 名駅太閤口店',
       lat: 35.169232,
-      lng: 36.881203
+      lng: 136.881203
     },
     {
       name: 'じーる',
@@ -69,17 +70,26 @@ $(function(){
    // 地図の作成
       var mapLatLng = new google.maps.LatLng({lat: markerData[0]['lat'], lng: markerData[0]['lng']}); // 緯度経度のデータ作成
      map = new google.maps.Map(document.getElementById('sponser-map'), { // #sampleに地図を埋め込む
-       center: mapLatLng, // 地図の中心を指定
-        zoom: 15 // 地図のズームを指定
+       center: {
+          lat: 35.1816645, // 緯度
+          lng: 136.9471074 // 経度
+       },
+        zoom: 9
      });
+
+    var icon = new google.maps.MarkerImage('images/common/icon-flag.png',
+      new google.maps.Size(40,58),/*アイコンサイズ設定*/
+      new google.maps.Point(0,0)/*アイコン位置設定*/
+    );
 
    // マーカー毎の処理
    for (var i = 0; i < markerData.length; i++) {
           markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
           marker[i] = new google.maps.Marker({ // マーカーの追加
-           position: markerLatLng, // マーカーを立てる位置を指定
-              map: map // マーカーを立てる地図を指定
-         });
+            position: markerLatLng, // マーカーを立てる位置を指定
+            icon: icon,
+            map: map // マーカーを立てる地図を指定
+          });
 
        infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
            content: '<div class="map-title">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
@@ -87,6 +97,21 @@ $(function(){
 
        markerEvent(i); // マーカーにクリックイベントを追加
    }
+
+   /*取得スタイルの貼り付け*/
+   var styleOptions = [
+     {
+       "stylers": [
+         { "hue": "#f08d24" },
+         { "visibility": "on" }
+       ]
+     }
+   ];
+   var styledMapOptions = { name: '芸工' }
+   var geikoMapType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+   map.mapTypes.set('sponser-map', geikoMapType);
+   map.setMapTypeId('sponser-map');
+
   }
 
   // マーカーにクリックイベントを追加
