10796454db39285e3a30f2fe58b1c59300fbefd4
加藤　優奈 <yuvernacrew@gmail.com>
2017-10-23 05:02:55 +0900

    headerのテンプレート化

---
+++ js/event.js
@@ -0,0 +1,29 @@
+$(function(){
+  $(window).on('hashchange',function(){
+    stageHashChecked();
+  });
+
+  $(window).on('load',stageHashChecked());
+
+  function stageHashChecked(){
+    switch(location.hash){
+      case "#stage":
+        $("#stage").attr("checked", true);
+        break;
+
+      case "#cafe":
+        $("#cafe").attr("checked", true);
+        break;
+
+      case "#ws":
+        $("#ws").attr("checked", true);
+        break;
+
+      case "#booth":
+        $("#booth").attr("checked", true);
+        break;
+      default:
+        break;
+    }
+  }
+})
--- js/script.js
+++ js/script.js
@@ -1,35 +1,6 @@
 $(function(){
-  $('#navToggle').click(function(){
-    $('.sp-header').toggleClass('open-nav');
-    $('.body-container').toggleClass('non-scroll');
-  });
-
-  $(window).on('hashchange',function(){
-    stageHashChecked();
-  });
-
-  $(window).on('load',stageHashChecked());
-
-
-  function stageHashChecked(){
-    switch(location.hash){
-      case "#stage":
-        $("#stage").attr("checked", true);
-        break;
-
-      case "#cafe":
-        $("#cafe").attr("checked", true);
-        break;
-
-      case "#ws":
-        $("#ws").attr("checked", true);
-        break;
-
-      case "#booth":
-        $("#booth").attr("checked", true);
-        break;
-      default:
-        break;
-    }
-  }
+  // sp navのcss変更
+  $.ajaxSetup({cache:false});
+  $("header").load("template/header.html");
+  $("footer").load("template/footer.html");
 })
